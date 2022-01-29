81a57d0092116e88a222da9ed51a37955011579c
valentinasv <valentinas.vaiceliunas@asseco.lt>
2022-01-29 22:42:46 +0200

    indicator

--- Assets/Vales_scene_stuff/ProgressBarManager.cs
+++ Assets/Vales_scene_stuff/ProgressBarManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ProgressBarManager : MonoBehaviour
 {
     private Slider slider;
+    public TextMeshProUGUI meterCount;
     public GameObject startingPoint;
     public GameObject endingPoint;
     public GameObject player;
@@ -16,7 +18,6 @@ public class ProgressBarManager : MonoBehaviour
         slider = gameObject.GetComponent<Slider>();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         totalDistance = (startingPoint.transform.position.y - endingPoint.transform.position.y);
@@ -24,13 +25,28 @@ public class ProgressBarManager : MonoBehaviour
 
     private void FixedUpdate()
     {
+        calculateProgressBar();
+    }
+
+    private void calculateProgressBar()
+    {
         float progress = 1 - ((player.transform.position.y - endingPoint.transform.position.y) / totalDistance);
-        if (slider.value < progress)
+        if (slider.value < progress || slider.value > progress)
         {
             slider.value = progress;
-        } else if (slider.value > progress)
-        {
-            slider.value = progress;
+            if (progress < 0)
+            {
+                meterCount.text = "Heaven";
+            }
+            else if (progress > 1)
+            {
+                meterCount.text = "The end";
+            }
+            else
+            {
+                meterCount.text = Mathf.Floor(progress * 1000).ToString() + "m";
+            }
+
         }
     }
 }
