d1c631c9281ab37eddf45f7f03bc115aed3d067f
Laurynas <laurynas.pupsta@gmail.com>
2022-01-29 17:24:03 +0200

    big scene changes

--- Assets/StartGameUIController.cs
+++ Assets/StartGameUIController.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class StartGameUIController : MonoBehaviour
 {
-    [SerializeField] private Animator startGamePlayerAnim;
-
-    public void StartGame() {
-        LevelManager.Instance.StartGame();
-        startGamePlayerAnim.SetTrigger("Jump");
-        Invoke("LoadGame", 2f);
-    }
-
-    public void LoadGame() {
-        Initiate.Fade("Aurio", Color.black, 2f);
+    public void PlayButtonPressed() {
+        MenuManager.Instance.StartGame();
     }
 }
